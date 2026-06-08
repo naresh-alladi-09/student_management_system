a7f5457d815dfdde57a0476cc5fbfb96cf0f3c7c
Alekhya <abandaru856@gmail.com>
2026-06-08 12:22:36 +0530

    attendence.css

--- src/pages/Attendence.jsx
+++ src/pages/Attendence.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "../styles/attendence.css";
 import { useEffect, useState } from "react";
 
+
 function Attendance() {
   const [students, setStudents] = useState([]);
 
@@ -10,35 +11,29 @@ function Attendance() {
     setStudents(data);
   }, []);
 
-  return (
-    <div>
-      <h2>Attendance Page</h2>
+ return (
+  <div>
+    <h2>Attendance Page</h2>
 
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Attendance</th>
+    <table border="1">
+      <thead>
+        <tr>
+          <th>Name</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {students.map((student, index) => (
+          <tr key={index}>
+            <td>{student.name}</td>
           </tr>
-        </thead>
-
-        <tbody>
-          {students.map((student, index) => (
-            <tr key={index}>
-              <td>{student.name}</td>
-
-              <td>
-                <select>
-                  <option>Present</option>
-                  <option>Absent</option>
-                </select>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 }
+
+
 
 export default Attendance;
