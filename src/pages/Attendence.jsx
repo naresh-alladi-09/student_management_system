2123b7ccaab4426ba7539457539056edb20baa35
Alekhya <abandaru856@gmail.com>
2026-06-08 12:13:58 +0530

    Updated Attendence

--- src/pages/Attendence.jsx
+++ src/pages/Attendence.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../styles/attendence.css";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function Attendance() {
   const [students, setStudents] = useState([]);
