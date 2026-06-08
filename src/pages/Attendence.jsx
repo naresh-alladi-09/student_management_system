5aa508065217c7e6ff129a0d73a3c8471cb04dcf
Alekhya <abandaru856@gmail.com>
2026-06-08 12:04:40 +0530

    Attendence

--- src/pages/Attendence.jsx
+++ src/pages/Attendence.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import "../styles/attendence.css";
+import React, { useEffect, useState } from "react";
 
-const Attendence = () => {
+function Attendance() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("students")) || [];
+    setStudents(data);
+  }, []);
+
   return (
-    <div>Attendence</div>
-  )
+    <div>
+      <h2>Attendance Page</h2>
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Attendance</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {students.map((student, index) => (
+            <tr key={index}>
+              <td>{student.name}</td>
+
+              <td>
+                <select>
+                  <option>Present</option>
+                  <option>Absent</option>
+                </select>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
-export default Attendence
+export default Attendance;
