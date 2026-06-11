f525719ab00a27f0b307f37636b6b711decf4cd9
Alekhya <abandaru856@gmail.com>
2026-06-11 21:47:27 +0530

    Updated Dashboardcard

--- src/components/Dashboardcard.jsx
+++ src/components/Dashboardcard.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
-const Dashboardcard = () => {
+import React from "react";
+import "./DashboardCard.css";
+function DashboardCard() {
   return (
-    <div>Dashboardcard</div>
-  )
+    <div className="dashboard-cards">
+      <div className="card">
+        <h3>Total Students</h3>
+        <h1>120</h1>
+      </div>
+
+      <div className="card">
+        <h3>Present Today</h3>
+        <h1>85</h1>
+      </div>
+
+      <div className="card">
+        <h3>Total Subjects</h3>
+        <h1>6</h1>
+      </div>
+
+      <div className="card">
+        <h3>Average Score</h3>
+        <h1>78%</h1>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboardcard
+export default DashboardCard;
