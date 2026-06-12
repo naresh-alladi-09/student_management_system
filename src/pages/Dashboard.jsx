f2223a9483bb214e9a2d4c3d3dcdbe1ad3b3eec3
Naresh <nareshalladi09@gmail.com>
2026-06-12 09:17:46 +0530

    errors done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ import Sidebar from "../components/Sidebar";
 import Dashboardcard from "../components/Dashboardcard";
 import Navbar from "../components/Navbar";
 import "../styles/dashboard.css";
+
+
 const Dashboard = () => {
   return (
     <div className="sideandmain">
@@ -12,6 +14,7 @@ const Dashboard = () => {
       <div className="main-content">
         <Navbar/>
         <Dashboardcard/>
+         
       </div>
     </div>
   )
