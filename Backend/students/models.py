b822cd168398d2d95bb21628d235afb90817ccf8
Alekhya <abandaru856@gmail.com>
2026-09-01 21:47:31 +0530

    k

--- Backend/students/models.py
+++ Backend/students/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name=models.CharField(max_length=100)
-    year=models.IntegerField(max_length=20)
+    year=models.IntegerField()
     email=models.EmailField()
     phone=models.CharField(max_length=15)
     branch=models.CharField(max_length=50)
